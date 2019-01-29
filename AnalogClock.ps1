2272f3088bfca78f7ad72ac460282542140ff7da
shigobu <32050537+shigobu@users.noreply.github.com>
2019-01-29 13:52:09 +0900

    Create AnalogClock.ps1

---
+++ AnalogClock.ps1
@@ -0,0 +1,9 @@
+# Get-Processコマンドレットでは、一つのプロセスを取得したときは配列では無いから、.net関数を使用した。
+$AnaPross = [System.Diagnostics.Process]::GetProcessesByName("アナログ時計WPF")
+$bool = $AnaPross.Length -gt 0
+if ($bool){
+    Write-Output "時計は起動中です。"
+}
+else{
+    Write-Output "時計を起動します"
+}
